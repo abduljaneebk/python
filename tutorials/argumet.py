c70e40c56395205afa601642edb2c44e09285755
abduljaneebk <abduljaneeb@gmail.com>
2024-12-14 12:35:31 +0530

    Add files via upload

---
+++ tutorials/argumet.py
@@ -0,0 +1,9 @@
+def sum(num1,num2):
+    sum=num1+num2
+num1=int(input("enter a number"))
+num2=int(input("enter a number"))
+    return sum
+        result=sum
+    print(result)
+
+    
---
+++ tutorials/sum using function.py
@@ -0,0 +1,6 @@
+def sum_of_numbers(num1,num2):
+    return num1+num2
+num1=int(input("enter two numbers : "))
+num2=int(input())
+result=sum_of_numbers(num1,num2)
+print("the result is : ",result)
