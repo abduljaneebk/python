55bb3ddf104eec1c8ccfd720351f5b5789f8dd40
abduljaneebk <abduljaneeb@gmail.com>
2024-12-16 15:30:45 +0530

    Add files via upload

---
+++ tutorials/sample scope.py
@@ -0,0 +1,13 @@
+def check_scope():
+    def do_local():
+        test="local test"
+    def non_local():
+        test="non local test"
+    def do_global():
+        test="global test"
+
+    test="default"
+    do_local()
+    print("test value after do local: "+test)
+
+check_scope()
